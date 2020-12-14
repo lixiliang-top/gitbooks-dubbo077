c09defb93dc7d69d519a704df94d0bcf4eb83126
lixiliang_glb <lixiliang_glb@kgc.com>
2020-12-14 18:56:59 +0800

    创建provider

---
+++ books-provider/src/main/java/cn/kgc/booksprovider/BooksProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.kgc.booksprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksProviderApplication.class, args);
+	}
+
+}
---
+++ books-provider/src/test/java/cn/kgc/booksprovider/BooksProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kgc.booksprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

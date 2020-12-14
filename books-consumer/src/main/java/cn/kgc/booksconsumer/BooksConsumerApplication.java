c8f044b9fd757be1f4d348f7f9e0df90272b6bf9
lixiliang_glb <lixiliang_glb@kgc.com>
2020-12-14 19:02:57 +0800

    创建consumer

---
+++ books-consumer/src/main/java/cn/kgc/booksconsumer/BooksConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.kgc.booksconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksConsumerApplication.class, args);
+	}
+
+}
---
+++ books-consumer/src/test/java/cn/kgc/booksconsumer/BooksConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kgc.booksconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
